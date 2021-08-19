ac751b79192baf54f79cf70664bd9282a9dcc40e
Lauren Goss <laurenlgoss98@gmail.com>
2021-08-19 18:52:22 -0400

    added html/api routes to server.js

--- server.js
+++ server.js
@@ -13,6 +13,20 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+app.get("/", (req, res) => {
 
+});
+
+app.get("/notes", (req, res) => {
+
+});
+
+app.get("/api/notes", (req, res) => {
+
+});
+
+app.post("/api/notes", (req, res) => {
+
+});
 
 app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
