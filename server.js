cfce0b4bc84905db25f466170970a998ba58f9bc
Lauren Goss <laurenlgoss98@gmail.com>
2021-08-21 17:20:52 -0400

    added html routes

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
-});
-
-app.get("/notes", (req, res) => {
-
-});
+app.get("/notes", (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 app.get("/api/notes", (req, res) => {
 
@@ -29,4 +29,6 @@ app.post("/api/notes", (req, res) => {
 
 });
 
-app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
+app.listen(PORT, () =>
+    console.log(`Application listening at http://localhost:${PORT}`)
+);
