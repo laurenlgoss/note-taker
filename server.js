66429c38809d2187da8f2893de43de57d2204a47
Lauren Goss <laurenlgoss98@gmail.com>
2021-08-22 12:46:08 -0400

    added post method for notes

--- server.js
+++ server.js
@@ -13,25 +13,69 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+// HTML route to index.html
 app.get("/", (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// HTML route to notes.html
 app.get("/notes", (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // GET request for notes
 app.get("/api/notes", (req, res) => {
-  // Inform the client
-  res.json(`${req.method} request received to retrieve notes`);
+    // Inform the client
+    res.json(`${req.method} request received to retrieve notes`);
 
-  // Log our request to the terminal
-  console.info(`${req.method} request received to retrieve notes`);
+    // Log GET request to terminal
+    console.info(`${req.method} request received to retrieve notes`);
 });
 
-app.post("/api/notes", (req, res) => {
+// Read data from file, append content
+const readAndAppend = (content, file) => {
+    fs.readFile(file, "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
 
+// Write content to JSON file
+const writeToFile = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    );
+
+// POST request for notes
+app.post("/api/notes", (req, res) => {
+    // Log POST request to terminal
+    console.info(`${req.method} request received to add a note`);
+
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+
+    // If all the required properties are present,
+    if (title && text) {
+        // Create variable for new note with unique ID
+        const newNote = {
+            title,
+            text,
+            note_id: uuid(),
+        }
+
+        // Add note to db.json
+        readAndAppend(newNote, "./db/db.json");
+
+        console.log(`Success! New note "${newNote.title}" posted.`);
+        res.json(newNote);
+    } else {
+        res.json("Error in posting note");
+    }
 });
 
 app.listen(PORT, () =>
